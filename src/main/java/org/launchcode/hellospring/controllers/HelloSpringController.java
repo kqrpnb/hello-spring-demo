d0d54072ed997d592883d00eb970b29c25c15315
Sean Duffy <kqrpnb@gmail.com>
2022-09-01 06:13:24 -0400

    Fnished 13.5 codealong

--- src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloSpringController.java
@@ -1,7 +1,11 @@
 package org.launchcode.hellospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -24,22 +28,59 @@ public class HelloSpringController {
 
     // Handles requests of the form /hello?name=LaunchCode
     @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST}, value = "hello")
-    @ResponseBody
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
+    public String hello(@RequestParam String name, Model model) {
+        String greeting = "Hello, " +
+                name +
+                "!";
+        model.addAttribute("greeting", greeting);
+        return "hello";
     }
 
     // Handles requests of the form /hello/LaunchCode
     @GetMapping("hello/{name}")
-    @ResponseBody
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
+    public String helloWithPathParam(@PathVariable String name, Model model) {
+        model.addAttribute("greeting", "Hello, " +
+                name +
+                "!");
+        return "hello";
     }
 
 
     @GetMapping("form")
     public String helloForm() {
         return "form";
+    }
+
+    @GetMapping("hello-names")
+    public String helloNames(Model model) {
+        List<String> names = new ArrayList<>();
+        names.add("LaunchCode");
+        names.add("Java");
+        names.add("JavaScript");
+        model.addAttribute("names", names);
+        return "hello-list";
+    }
+
+    @GetMapping("hello-numbers")
+    public String helloNumbers(Model model) {
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(-1);
+        numbers.add(12);
+        numbers.add(-21);
+        numbers.add(87);
+        numbers.add(42);
+        numbers.add(0);
+        numbers.add(172);
+        numbers.add(121);
+        numbers.add(155);
+        numbers.add(-92);
+        numbers.add(9);
+        numbers.add(2);
+        numbers.add(12);
+        numbers.add(-91);
+        model.addAttribute("numbers", numbers);
+        return "hello-numbers";
     }
 
 }
